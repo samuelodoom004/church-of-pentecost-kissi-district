a97c3919feb5ad954f79a64b1f0b5e3b2fa5c2e3
samuelodoom004 <samuel.odoom004@stu.ucc.edu.gh>
2025-08-21 21:31:16 +0000

    Add files via upload

---
+++ sam.js
@@ -0,0 +1,29 @@
+/*const cars = [ "Audi", "Benz", "BMW"];
+cars [0] = "Toyota";
+cars.push ("Audi")
+document.getElementById("sam").innerHTML = cars;
+
+const car = {type: "Fiat", model:"500", color: "white"};
+car.color = "red";
+car.owner = "Johnson";
+
+document.getElementById ("header").innerHTML = "car owner is "  + car.owner;
+
+let x=5;
+let y=10;
+let z=x*y;
+
+document.getElementById ("ham").innerHTML = "x is equal to " + z;
+
+let text1 ="Ama like Bathing";
+let text2 ="kojo like Bathing";
+let result = text1 > text2;
+
+document.getElementById ("jame").innerHTML = "Ama like bathing " + result;
+*/
+
+true;
+
+console.log(true);
+document.getElementById ("odoom").innerHTML = console.log;
+
